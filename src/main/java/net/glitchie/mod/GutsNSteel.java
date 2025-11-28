54aeb22764ee3a142cff55e2b199c0cb6188161a
Glitchie-V03 <glitchy20119@gmail.com>
2025-11-28 16:10:04 +0000

    innitial commit

---
+++ src/main/java/net/glitchie/mod/GutsNSteel.java
@@ -0,0 +1,16 @@
+package net.glitchie.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GutsNSteel implements ModInitializer {
+	public static final String MOD_ID = "guts-n-steel";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/glitchie/mod/GutsNSteelClient.java
@@ -0,0 +1,10 @@
+package net.glitchie.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GutsNSteelClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
